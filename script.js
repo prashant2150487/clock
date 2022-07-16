3c34fa010e32b9f69ad77193b9ddedf8f7b8b197
prashant2150487 <you@example.com>
2022-07-16 22:42:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let date;
+let time;
+const option = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+setInterval(() => {
+  a = new Date();
+  time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+  date = a.toLocaleDateString(undefined, option);
+  document.getElementById("time").innerHTML = time + " on " + date;
+}, 1000);
